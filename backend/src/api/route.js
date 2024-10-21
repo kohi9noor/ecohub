f42a0e845a7337a7a9c5d505d51bbaf904d89ade
kohinoor-nimes <kohinoornimes@gmail.com>
2024-10-22 00:08:37 +0530

    Delete backend directory

--- backend/src/api/route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const userStateRoutes = require("./user-state/route");
-const dashboardRouets = require("./dashboard/route");
-
-router.use("/api", userStateRoutes);
-router.use("/api", dashboardRouets);
-module.exports = router;
